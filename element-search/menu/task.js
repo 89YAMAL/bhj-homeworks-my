a43a52425a1ae697e3e01dc5ae8899bf15c433f2
89YAMAL <alex-popov1987@yandex.ru>
2022-07-19 10:18:30 +0500

    version 4

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,17 +4,34 @@ let arrLinks = Array.from(links);
 
 for (let i = 0; i < arrLinks.length; i++) {
   arrLinks[i].onclick = function () {
-
-    if (arrLinks[i].parentElement.querySelector('.menu_sub').className === "menu menu_sub") {
-      arrLinks[i].parentElement.querySelector('.menu_sub').className = "menu menu_sub menu_active";
-
-    } else if (arrLinks[i].parentElement.querySelector('.menu_sub').className ==="menu menu_sub menu_active"){
-      arrLinks[i].parentElement.querySelector('.menu_sub').className = "menu menu_sub";
+    if (arrLinks[i].nextElementSibling.className === "menu menu_sub") {
+      arrLinks[i].nextElementSibling.className = "menu menu_sub menu_active";
+    } else {
+      arrLinks[i].target.nextElementSibling.className = "menu menu_sub";
     }
 
     if (arrLinks[i].closest('.menu_main')) {
       return false;
     }
-
   }
 }
+
+
+
+
+
+
+
+
+    // if (arrLinks[i].parentElement.querySelector('.menu_sub').className === "menu menu_sub") {
+    //   arrLinks[i].parentElement.querySelector('.menu_sub').className = "menu menu_sub menu_active";
+    // } else {
+    //   arrLinks[i].parentElement.querySelector('.menu_sub').className = "menu menu_sub";
+    // }
+    //
+    // if (arrLinks[i].closest('.menu_main')) {
+    //   return false;
+    // }
+
+//   }
+// }
